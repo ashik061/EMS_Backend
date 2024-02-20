d03655bb8271c04fc6dcce6405be5df68436e1bb
ashik061 <ashikurrahman.24csedu.061@gmail.com>
2024-02-20 19:37:45 +0600

    Successfully created a test API

--- src/main/java/com/dev/EmsBackend/EmsBackendApplication.java
+++ src/main/java/com/dev/EmsBackend/EmsBackendApplication.java
@@ -1,21 +1,20 @@
 package com.dev.EmsBackend;
 
+import com.dev.EmsBackend.role.Role;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @SpringBootApplication
-@RestController
 public class EmsBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmsBackendApplication.class, args);
 	}
 
-	@GetMapping
-	public String hello(){
-		return "Hello World !";
-	}
+
 
 }
